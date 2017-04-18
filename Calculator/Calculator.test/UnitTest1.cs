19c1f5285d36da6d46760f94a7355e5a1cba2a73
shantilal <shantilal.mali@netweb.biz>
2017-04-18 10:28:47 +0530

    first commit

---
+++ Calculator/Calculator.test/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using Calculator;
+namespace Calculator.test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestCase]
+        public void AddTest()
+        {
+            MathsHelper helper = new MathsHelper();
+            int result = helper.Add(20, 10);
+            NUnit.Framework.Assert.AreEqual(30, result);
+        }
+
+        //[TestCase]
+        //public void SubtractTest()
+        //{
+        //    MathsHelper helper = new MathsHelper();
+        //    int result = helper.Subtract(20, 10);
+        //    NUnit.Framework.Assert.AreEqual(10, result);
+        //}
+    }
+}
